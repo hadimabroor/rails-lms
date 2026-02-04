983d31727e1d0c2fca374e1dd2c0f3d2bff5982f
Hadi Mabroor <42314590+hadimabroor@users.noreply.github.com>
2026-02-05 00:52:40 +0530

    Create Enrollment model with associations and uniqueness validation

---
+++ app/models/enrollment.rb
@@ -0,0 +1,6 @@
+class Enrollment < ApplicationRecord
+  belongs_to :user
+  belongs_to :course
+
+  validates :user_id, uniqueness: { scope: :course_id, message: 'has already enrolled in this course' }
+end
